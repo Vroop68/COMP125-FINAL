34efe38c9817ee7e23c55d7a4a0e745de37a1dea
John Knoop <john.r.knoop@gmail.com>
2018-08-15 11:49:29 -0400

    updated addressbook and contacts constructor

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,5 +1,27 @@
 (function(){
 
+    function loadJSON() {
+    
+        XHR = new XMLHttpRequest();
+        XHR.addEventListener("readystatechange", function(){
+          if(this.status === 200) {
+            if(this.readyState === 4)  {
+              addressBook = JSON.parse(this.responseText);
+              console.log("Data finished loading");
+    
+              add();
+    
+              //displayData();
+            }
+          }
+        });
+        XHR.open("GET", "/data.json");
+        XHR.send();
+
+      }
+      
+      
+
     function Start() {
         console.log(`%c App Started...`,"color:blue; font-size: 30px; font-weight:bold;");
        // Your Code goes here
--- public/Scripts/objects/addressbook.js
+++ public/Scripts/objects/addressbook.js
@@ -3,8 +3,17 @@
         constructor() {
             this.Contacts = [];
         }
-
+        
         // complete the addressbook class
+        add(){
+            function createContacts() {
+                AddressBook.Contacts.forEach(contact => {
+                  let newContact = new objects.Contact(
+                    contact.firstName, contact.lastName, contact.contactNumber, contact.emailAddress);
+                  Contacts.push(newContact);
+                });
+              }
+        }
     }
 
     objects.AddressBook = AddressBook;
